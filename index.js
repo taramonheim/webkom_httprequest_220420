1b0e4219162591457313bfb2cebb9a2550c57239
taramonheim <taramonheim@me.com>
2020-06-04 12:54:47 +0200

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const mongo = require('mongodb').MongoClient;
 
-const uri = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/men sa.json';
+const uri = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
 
 
 // Library inits
